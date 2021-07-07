c71521f0ea81533887f54b333feb0fb0f60b06de
Laura Jacquin <laura.jcqn@live.fr>
2021-07-07 17:34:50 +0200

    priorities for division and multiplication

--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 class Calculator {
+
+    // MARK: - Private variables
+
     private(set) var currentExpression: String = "" {
         didSet {
             sendCurrentExpressionDidChangeNotification()
@@ -34,6 +37,8 @@ class Calculator {
     private var expressionIsCorrect: Bool {
         return elements.last != "+" && elements.last != "-" && elements.last != "*" && elements.last != "/"
     }
+
+    // MARK: - Functions
 
     func addNumber(_ number: Int) {
         if expressionHaveResult {
@@ -83,57 +88,94 @@ class Calculator {
             // result is already calculated
             return true
         }
-
         guard expressionHaveEnoughElement else {
             return false
         }
-
         guard expressionIsCorrect else {
             return false
         }
 
-        // Create local copy of operations
-        var operationsToReduce = elements
-
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 {
-            guard let left = Double(operationsToReduce[0]) else {
-                return false
-            }
-
-            let operand = operationsToReduce[1]
-
-            guard let right = Double(operationsToReduce[2]) else {
-                return false
-            }
-
-            if operand.elementsEqual("/") && right == 0 {
-                return false
-            }
-
-            let result: Double
-            switch operand {
-            case "+": result = left + right
-            case "-": result = left - right
-            case "x": result = left * right
-            case "/": result = left / right
-            default: return false
-            }
-
-            // rounded result to avoid displaying too many numbers :
-            let resultToDisplay = (result * 100).rounded() / 100
-
-            operationsToReduce = Array(operationsToReduce.dropFirst(3))
-            operationsToReduce.insert("\(resultToDisplay)", at: 0)
+        guard let reducedPriority = reducePriorityOperations(elements: elements) else {
+            return false
         }
 
-        guard let result = operationsToReduce.first else {
+        guard let reducedBasic = reduceBasicOperations(reducedPriority) else {
+            return false
+        }
+
+        guard let result = reducedBasic.first else {
             return false
         }
 
         currentExpression.append(" = \(result)")
 
         return true
+    }
+
+    private func reduceBasicOperations(_ elements: [String]) -> [String]? {
+        // Create local copy of operations :
+        var localElements = elements
+
+        // Iterate over operations while an operand still here :
+        while localElements.count > 1 {
+            guard let left = Double(localElements[0]) else {
+                return nil
+            }
+
+            let operand = localElements[1]
+
+            guard let right = Double(localElements[2]) else {
+                return nil
+            }
+
+            let result: Double
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            default: return nil
+            }
+
+            // rounded result to avoid displaying too many numbers :
+            let resultToDisplay = (result * 100).rounded() / 100
+
+            localElements = Array(localElements.dropFirst(3))
+            localElements.insert("\(resultToDisplay)", at: 0)
+        }
+
+        return localElements
+    }
+
+    func reducePriorityOperations(elements: [String]) -> [String]? {
+        var localElements = elements
+        while localElements.contains("x") || localElements.contains("/") {
+            guard let index = localElements.firstIndex(where: { $0 == "x" || $0 == "/" }) else {
+                return nil
+            }
+            guard let left = Double(localElements[index-1]) else {
+                return nil
+            }
+            guard let right = Double(localElements[index+1]) else {
+                return nil
+            }
+            let operand = localElements[index]
+            // Division by 0 :
+            if operand == "/" && right == 0 {
+                return nil
+            }
+            let result: Double
+            switch operand {
+                case "x": result = left * right
+                case "/": result = left / right
+                default : return nil
+            }
+            for _ in 1...3 {
+                // Remove the 3 elements at the lowest index
+                localElements.remove(at: index-1)
+            }
+            // Insert result of reduced operation at the lowest index
+            localElements.insert("\(result)", at: index-1)
+        }
+        return localElements
     }
 
     func sendCurrentExpressionDidChangeNotification() {
